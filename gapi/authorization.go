39a07e6f25e1b2148134d562a6bc8a6d69c4734f
Fernando Segovia <fernandosegovia25@gmail.com>
2023-01-24 08:09:26 -0600

    Fix warning

---
+++ gapi/authorization.go
@@ -0,0 +1,42 @@
+package gapi
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/ferseg/golang-simple-bank/token"
+	"google.golang.org/grpc/metadata"
+)
+
+const (
+  authorizationHeader = "authorization"
+  authorizationBearer = "bearer"
+)
+
+func (server *Server) authorizeUser(ctx context.Context) (*token.Payload, error) {
+  md, ok := metadata.FromIncomingContext(ctx)
+  if ! ok {
+    return nil, fmt.Errorf("Missing metadata")
+  }
+
+  values := md.Get(authorizationHeader)
+  if len(values) == 0 {
+    return nil, fmt.Errorf("Missing auth header")
+  }
+  authHeader := values[0]
+  fields := strings.Fields(authHeader)
+  if len(fields) < 2 {
+    return nil, fmt.Errorf("Invalid auth header format")
+  }
+  authType := strings.ToLower(fields[0])
+  if authType != authorizationBearer {
+    return nil, fmt.Errorf("Unsupported authorization type: %s", authType)
+  }
+  accessToken := fields[1]
+  payload, err := server.tokenMaker.VerifyToken(accessToken)
+  if err!= nil {
+    return nil, fmt.Errorf("The token is invalid: %s", err)
+  }
+  return payload, nil
+}
